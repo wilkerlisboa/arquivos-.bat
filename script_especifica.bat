0aac90bdf2333926952693c243670ddd0278bcf2
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2023-03-10 15:05:05 -0300

    Add files via upload

---
+++ script_especifica.bat
@@ -0,0 +1,82 @@
+mkdir professor
+cd professor 
+mkdir disciplina1
+mkdir disciplina2
+mkdir disciplina3
+mkdir disciplina4
+mkdir disciplina5
+
+cd disciplina1
+mkdir unidade1 
+mkdir unidade2 
+mkdir unidade3
+mkdir avs
+
+cd unidade1
+mkdir at 
+cd ..
+cd unidade2
+mkdir at 
+cd..
+cd unidade3
+mkdir at
+cd ..
+cd avs 
+mkdir av1 av2 av3 recupera
+cd ..\..
+cd disciplina2 
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1
+mkdir at
+cd ..
+cd unidade2
+mkdir at 
+cd ..
+cd unidade3
+mkdir at
+cd ..
+cd avs 
+mkdir av1 av2 av3 recupera
+cd ..\..
+cd disciplina3
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1
+mkdir at
+cd ..
+cd unidade2
+mkdir at
+cd ..
+cd unidade3
+mkdir at
+cd ..
+cd avs 
+mkdir av1 av2 av3 recupera
+cd ..\..
+cd disciplina4
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1
+mkdir at
+cd ..
+cd unidade2
+mkdir at
+cd ..
+cd unidade3
+mkdir at
+cd ..
+cd avs
+mkdir av1 av2 av3 recupera
+cd ..\.. 
+cd disciplina5
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1
+mkdir at
+cd ..
+cd unidade2
+mkdir at
+cd ..
+cd unidade3
+mkdir at
+cd ..
+cd avs
+mkdir av1 av2 av3 recupera
+pause
