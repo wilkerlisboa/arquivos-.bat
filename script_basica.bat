902738022c826024e35d2c141029907bfac81ca7
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2023-03-10 14:48:39 -0300

    Add files via upload

---
+++ script_basica.bat
@@ -0,0 +1,75 @@
+mkdir C:\Users\bootR\Desktop\professor 
+start C:\Users\bootR\Desktop\professor
+mkdir C:\Users\bootR\Desktop\professor\disciplina1
+mkdir C:\Users\bootR\Desktop\professor\disciplina2
+mkdir C:\Users\bootR\Desktop\professor\disciplina3
+mkdir C:\Users\bootR\Desktop\professor\disciplina4
+mkdir C:\Users\bootR\Desktop\professor\disciplina5
+
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\unidade1
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\unidade2
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\unidade3
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\avs
+
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\unidade1\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\unidade2\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\unidade3\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\avs\av1
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\avs\av2
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\avs\av3
+mkdir C:\Users\bootR\Desktop\professor\disciplina1\avs\recupera
+// DISCIPLINA 02
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\unidade1
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\unidade2
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\unidade3
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\avs
+
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\unidade1\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\unidade2\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\unidade3\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\avs\av1
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\avs\av2
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\avs\av3
+mkdir C:\Users\bootR\Desktop\professor\disciplina2\avs\recupera
+
+// DISCIPLINA 03
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\unidade1
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\unidade2
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\unidade3
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\avs
+
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\unidade1\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\unidade2\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\unidade3\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\avs\av1
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\avs\av2
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\avs\av3
+mkdir C:\Users\bootR\Desktop\professor\disciplina3\avs\recupera
+
+//DISCIPLINA 04
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\unidade1
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\unidade2
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\unidade3
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\avs
+
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\unidade1\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\unidade2\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\unidade3\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\avs\av1
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\avs\av2
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\avs\av3
+mkdir C:\Users\bootR\Desktop\professor\disciplina4\avs\recupera
+
+//DISCIPLINA 05
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\unidade1
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\unidade2
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\unidade3
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\avs
+
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\unidade1\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\unidade2\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\unidade3\at
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\avs\av1
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\avs\av2
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\avs\av3
+mkdir C:\Users\bootR\Desktop\professor\disciplina5\avs\recupera
