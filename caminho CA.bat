b416a0c6bbd92d62fcbd2bbcae2e65e171eac6db
SeuNome <seuEmail@gmail.com>
2023-03-30 08:32:35 -0300

    caminho ABSOLUTO

---
+++ caminho CA.bat
@@ -0,0 +1,75 @@
+mkdir Professor 
+cd Professor
+mkdir Disciplina1 Disciplina2 Disciplina3 Disciplina4
+cd Disciplina1 
+mkdir unidade1 unidade2 unidade3 avs 
+cd unidade1 
+mkdir at 
+cd ..\..
+cd Disciplina1
+cd unidade2
+mkdir at
+cd ..\..
+cd Disciplina1
+cd unidade3
+mkdir at
+cd ..\..
+cd Disciplina1
+cd avs
+mkdir av1 av2 av3 recupera
+cd ..\..
+cd Disciplina2
+mkdir unidade1 unidade2 unidade3 avs 
+cd unidade1
+mkdir at
+cd ..\..
+cd Disciplina2 
+cd unidade1
+mkdir at
+cd ..\..
+cd Disciplina2 
+cd unidade2
+mkdir at 
+cd ..\..
+cd Disciplina2 
+cd unidade3
+mkdir at 
+cd ..\..
+cd Disciplina2 
+cd avs
+mkdir av1 av2 av3 recupera 
+cd ..\..
+cd Disciplina3
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1
+mkdir at 
+cd ..\.. 
+cd Disciplina3 
+cd unidade2
+mkdir at
+cd ..\..
+cd Disciplina3
+cd unidade3
+mkdir at
+cd ..\..
+cd Disciplina3
+cd avs
+mkdir av1 av2 av3 recupera 
+cd ..\..
+cd Disciplina4
+mkdir unidade1 unidade2 unidade3 avs
+cd unidade1
+mkdir at 
+cd ..\..
+cd Disciplina4
+cd unidade2
+mkdir at
+cd ..\..
+cd Disciplina4
+cd unidade3
+mkdir at
+cd ..\..
+cd Disciplina4
+cd avs
+mkdir av1 av2 av3 recupera
+pause
