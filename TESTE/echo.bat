9464ae469ae4acb320e681969292334e6c049c67
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2023-06-04 01:08:39 -0300

    hours

--- TESTE/echo.bat
+++ TESTE/echo.bat
@@ -10,6 +10,16 @@ echo #    bem vindo %nome%   #
 echo #                       #
 echo #########################
 
+set caminhoOrigem="C:\Users\bootR\Desktop\dados"
+
+set caminhoDestino="C:\Users\bootR\Desktop\servidor"
+
+set fileOne="C:\Users\bootR\Desktop\servidor\roberto"
+
+set fileTwo="C:\Users\bootR\Desktop\servidor\wilker"
+
+set fileWithData="C:\Users\bootR\Desktop\servidor\%data%"
+
 set data=%date:~6,4%-%date:~3,2%-%date:~0,2%
 
 set hora=%time:~0,2%:%time:~3,2%
@@ -18,17 +28,17 @@ echo Data atual: %data%
 
 echo Hora Atual: %hora%
 
-if "%hora%" == %time:~3,31% (goto pasta)
+if "%hora%" == %time:~0,5% (goto pasta)
 
 :pasta
  cd servidor
  if not exist %data% md %data%
- xcopy "C:\Users\bootR\Desktop\dados" "C:\Users\bootR\Desktop\servidor" /A /S /E /Y
+ xcopy "%caminhoOrigem%" "%caminhoDestino%" /A /S /E /Y
 goto segunda
 
 :segunda
- move "C:\Users\bootR\Desktop\servidor\roberto" "C:\Users\bootR\Desktop\servidor\%data%"
- move "C:\Users\bootR\Desktop\servidor\wilker" "C:\Users\bootR\Desktop\servidor\%data%"
+ move "%fileOne%" "%fileWithData%"
+ move "%fileTwo%" "%fileWithData%"
 goto finalizado
 :finalizado
 pause
